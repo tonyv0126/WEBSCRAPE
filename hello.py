b123255af7a9787f44786ae2458a8ed01cb5ab01
tonyvu <tonyvu0126@gmail.com>
2022-11-11 10:20:53 +1100

    forth

--- hello.py
+++ hello.py
@@ -1,13 +1,12 @@
 print("Hello World")
 print("Hello World 2")
-<<<<<<< HEAD
 print("Hello World 3")
+print("Hello World 4")
 
 
-=======
+
 
 z=5
 y=5
 A=1
 print(z+y)
->>>>>>> d52773e71aedcb29658632dd444be33aa2a1d620
